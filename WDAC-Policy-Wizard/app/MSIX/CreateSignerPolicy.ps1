1cc07f2ce3ed8df08d8edb4220bdd601735b640a
Jordan Geurten <jogeurte@microsoft.com>
2024-05-17 16:56:12 -0400

    Publish V2.4.3.0 built off .NET Core 8

---
+++ WDAC-Policy-Wizard/app/MSIX/CreateSignerPolicy.ps1
@@ -0,0 +1,33 @@
+﻿# WdacBinPath: path to the WDACWizard.exe; known to be on the disk
+# DriverFilePath: path to the binary to be scanned for TBS hash rule
+# PolicyPath: path where to create the new policy XML
+# Level: Rule level (e.g. Publisher, PcaCertificate,Hash)
+param (
+    [string]$WdacBinPath,
+    [string]$DriverFilePath,
+    [string]$PolicyPath,
+    [string]$Level,
+    [string]$Deny
+)
+
+# Expected to fail due to CmdletInvocationException error. Hresult = -2146233087
+try
+{
+    $DummySignerRule = New-CIPolicyRule -Level Publisher -DriverFilePath $WdacBinPath -Fallback Hash
+}
+catch
+{
+}
+
+# Run New-CIPolicyRule to generate a rule object from the provided driver file path and specified level (Publisher vs PcaCertificate)
+if($Deny -eq "False")
+{
+    $SignerRule = New-CIPolicyRule -Level $Level -DriverFilePath $DriverFilePath -Fallback Hash -Deny
+}
+else
+{
+    $SignerRule = New-CIPolicyRule -Level $Level -DriverFilePath $DriverFilePath -Fallback Hash
+}
+
+# Create policy from the SignerRule object
+New-CIPolicy -Rules $SignerRule -FilePath $PolicyPath
