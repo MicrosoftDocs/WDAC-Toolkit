de6855adafa7faae0e7fe1dbe7186a95a996add3
Jordan Geurten <jogeurte@microsoft.com>
2024-06-14 11:37:44 -0400

    Fixed incorrect logic in PowerShell script (#388)

--- WDAC-Policy-Wizard/app/MSIX/CreateSignerPolicy.ps1
+++ WDAC-Policy-Wizard/app/MSIX/CreateSignerPolicy.ps1
@@ -11,6 +11,7 @@ param (
 )
 
 # Expected to fail due to CmdletInvocationException error. Hresult = -2146233087
+# This fixes the KeyNotFound error and frees the error for the subsequent New-CiPolicyRule cmd
 try
 {
     $DummySignerRule = New-CIPolicyRule -Level Publisher -DriverFilePath $WdacBinPath -Fallback Hash
@@ -22,11 +23,11 @@ catch
 # Run New-CIPolicyRule to generate a rule object from the provided driver file path and specified level (Publisher vs PcaCertificate)
 if($Deny -eq "False")
 {
-    $SignerRule = New-CIPolicyRule -Level $Level -DriverFilePath $DriverFilePath -Fallback Hash -Deny
+    $SignerRule = New-CIPolicyRule -Level $Level -DriverFilePath $DriverFilePath -Fallback Hash
 }
 else
 {
-    $SignerRule = New-CIPolicyRule -Level $Level -DriverFilePath $DriverFilePath -Fallback Hash
+    $SignerRule = New-CIPolicyRule -Level $Level -DriverFilePath $DriverFilePath -Fallback Hash -Deny
 }
 
 # Create policy from the SignerRule object
