ac4e64b7ac835cdba26cd22f999959bb2a35b441
Jordan Geurten <jogeurte@microsoft.com>
2024-10-11 17:53:57 -0400

    Fixed merge conflict

--- WDAC-Policy-Wizard/app/src/MainForm.cs
+++ WDAC-Policy-Wizard/app/src/MainForm.cs
@@ -1409,17 +1409,23 @@ namespace WDAC_Wizard
             // Check whether the User Mode recommended block list rules are wanted in the output:
             if(this.Policy.UseUserModeBlocks)
             {
-                siPolicyCustomRules = PolicyHelper.MergePolicies(siPolicyCustomRules,
-                                                                 Helper.DeserializeXMLStringtoPolicy(
-                                                                     Path.Combine(this.ExeFolderPath, "Recommended_UserMode_Blocklist.xml")));
+                // Instead, copy to the temp folder 
+                string umBlocklist_src = Path.Combine(this.ExeFolderPath, "Recommended_UserMode_Blocklist.xml");
+                string umBlocklist_dst = Path.Combine(this.TempFolderPath, "Recommended_UserMode_Blocklist.xml");
+                File.Copy(umBlocklist_src, umBlocklist_dst, true);
+
+                siPolicyCustomRules = PolicyHelper.MergePolicies(siPolicyCustomRules, Helper.DeserializeXMLtoPolicy(umBlocklist_dst));
             }
 
             // Check whether the Kernel Mode recommended driver block list rules are wanted in the output:
             if (this.Policy.UseKernelModeBlocks)
             {
-                siPolicyCustomRules = PolicyHelper.MergePolicies(siPolicyCustomRules,
-                                                                 Helper.DeserializeXMLStringtoPolicy(
-                                                                     Path.Combine(this.ExeFolderPath, "Recommended_KernelMode_Blocklist.xml"))); 
+                // Instead, copy to the temp folder 
+                string kmBlocklist_src = Path.Combine(this.ExeFolderPath, "Recommended_Driver_Blocklist.xml");
+                string kmBlocklist_dst = Path.Combine(this.TempFolderPath, "Recommended_Driver_Blocklist.xml");
+                File.Copy(kmBlocklist_src, kmBlocklist_dst, true);
+
+                siPolicyCustomRules = PolicyHelper.MergePolicies(siPolicyCustomRules,Helper.DeserializeXMLtoPolicy(kmBlocklist_dst)); 
             }
 
             // New Policies:
