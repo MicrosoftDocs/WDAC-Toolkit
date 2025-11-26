5c38bf916ad4062c855fc584b61719faa123347a
Jordan Geurten <jogeurte@microsoft.com>
2025-11-25 19:02:40 -0500

    Added a check for empty Issuing TBS hash rules to block rule creation

--- WDAC-Policy-Wizard/app/src/EventLogRuleConfiguration.cs
+++ WDAC-Policy-Wizard/app/src/EventLogRuleConfiguration.cs
@@ -354,8 +354,10 @@ namespace WDAC_Wizard
             this.PublisherUIState[3] = this.versionCheckBox.Checked == true ? 1 : 0;
             this.PublisherUIState[4] = this.productCheckBox.Checked == true ? 1 : 0;
 
-            if(String.IsNullOrEmpty(this.issuerTextBox.Text) 
+            // Check the issuing CA is not empty AND Issuer TBS hash not empty
+            if (String.IsNullOrEmpty(this.issuerTextBox.Text) 
                 || this.issuerTextBox.Text == Properties.Resources.BadEventPubValue)
+                
             {
                 // Log exception error and throw error to user
                 MessageBox.Show(this,String.Format("The Issuer is invalid for rule creation. The issuer cannot be empty or '{0}'", Properties.Resources.BadEventPubValue), 
@@ -368,6 +370,23 @@ namespace WDAC_Wizard
                 Logger.Log.AddWarningMsg("Event Log Config - Invalid publisher rule with Issuer = Unknown");
                 return false; 
             }
+
+            if ( this.CiEvents[this.SelectedRow].SignerInfo.IssuerTBSHash.Length == 0)
+            // Check the issuing CA is not empty AND Issuer TBS hash not empty
+            // Instances where the issuing CA is in the data but not the TBS hash. GitHub Issue #478
+            {
+                // Log exception error and throw error to user
+                MessageBox.Show(this, "The Issuer is invalid for rule creation. The Issuer TBS hash value is empty. This is likely an issue in the Advanced Hunting data.",
+                                 "New Rule Creation Error",
+                                 MessageBoxButtons.OK,
+                                 MessageBoxIcon.Error);
+
+                this.Invalidate();
+
+                Logger.Log.AddWarningMsg("Event Log Config - Invalid publisher rule with a null Issuer TBS Hash");
+                return false;
+            }
+
 
             if (PublisherUIState[1] == 1 && (String.IsNullOrEmpty(this.publisherTextBox.Text) 
                 || this.publisherTextBox.Text == Properties.Resources.BadEventPubValue))
